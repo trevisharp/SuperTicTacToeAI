ddcb2703552f2d95b4c26061c93c8592f5dbfdfa
Trevisan <leonardosilio@alunos.utfpr.edu.br>
2022-04-23 03:23:19 -0300

    Implementation of Animation of UI Grid

--- ST3AI/State.cs
+++ ST3AI/State.cs
@@ -3,11 +3,24 @@ public class State
     internal ushort[] data;
     internal State() { }
 
+    public Piece this[int collumn, int row, int i, int j]
+        => (Piece)data[3 * row + collumn].GetPiece(i, j);
+
     public void Play(int collumn, int row, int i, int j, Piece piece)
     {
         ushort move = piece == Piece.Default ?
             (ushort)(data[0] % 4) : (ushort)piece;
         var section = data[3 * row + collumn];
 
+    }
+
+    public static State Empty
+    {
+        get
+        {
+            State state = StatePool.Current.RentState();
+            state.data[0] = 1;
+            return state;
+        }
     }
 }
--- ST3UI/Program.cs
+++ ST3UI/Program.cs
@@ -11,6 +11,8 @@ Application.SetCompatibleTextRenderingDefault(false);
 
 var form = new Form();
 
+State state = State.Empty;
+
 form.WindowState = FormWindowState.Maximized;
 form.FormBorderStyle = FormBorderStyle.FixedToolWindow;
 form.Text = "Super Tic Tac Toe Artificial Inteligence";
@@ -21,6 +23,7 @@ form.Controls.Add(pb);
 
 Bitmap bmp = null;
 Graphics g = null;
+int frame = 0;
 
 Timer tm = new Timer();
 tm.Interval = 25;
@@ -34,9 +37,90 @@ form.Load += delegate
 
 tm.Tick += delegate
 {
+    frame++;
     g.Clear(Color.White);
+
+    float wid = bmp.Width,
+          hei = bmp.Height;
+    
+    float majorgridwidth = 10f;
+    float minorgridwidth = 5f;
+    float majorsize = .3f * hei - 2 * majorgridwidth;
+    float minorsize = .3f * majorsize - 2 * minorgridwidth;
+    float gridstart = (wid - hei) / 2f;
+    float padding = .05f * hei;
+    float minorpadding = .05f * majorsize;
+
+    #region Start Grid Animation
+
+    float initstate = (frame > 40 ? 40 : frame) / 40f;
+
+    animategrid(gridstart, 0, padding, majorsize, majorgridwidth, initstate, Brushes.Black);
+    
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            animategrid(gridstart + padding + i * majorsize,
+                padding + j * majorsize,
+                minorpadding, minorsize, minorgridwidth,
+                initstate, Brushes.Black);
+        }
+    }
+
+    if (frame < 40) //initialization fase
+    {
+        pb.Image = bmp;
+        return;
+    }
+    
+    #endregion
+
+    g.DrawString(state[0, 0, 0, 0].ToString(), form.Font, Brushes.Black, PointF.Empty);
 
     pb.Image = bmp;
 };
+
+void animategrid(
+    float startx, float starty, 
+    float padding, float size, float barwidth, 
+    float animationpercent, Brush color)
+{
+    float totalsize = 3 * size + 2 * padding;
+    animatebar(
+        startx + padding + size - barwidth / 2, starty + totalsize / 2,
+        barwidth, 0, 
+        barwidth, 3 * size,
+        animationpercent, color);
+    
+    animatebar(
+        startx + padding + 2 * size - barwidth / 2, starty + totalsize / 2,
+        barwidth, 0, 
+        barwidth, 3 * size,
+        animationpercent, color);
+    
+    animatebar(
+        startx + totalsize / 2, starty + padding + size - barwidth / 2,
+        0, barwidth, 
+        3 * size, barwidth,
+        animationpercent, color);
+    
+    animatebar(
+        startx + totalsize / 2, starty + padding + 2 * size - barwidth / 2,
+        0, barwidth, 
+        3 * size, barwidth,
+        animationpercent, color);   
+}
+
+void animatebar(
+    float startx, float starty,
+    float startwid, float starthei,
+    float maxwid, float maxhei,
+    float animationpercent, Brush color)
+{
+    float wid = (maxwid - startwid) * animationpercent + startwid;
+    float hei = (maxhei - starthei) * animationpercent + starthei;
+    g.FillRectangle(color, startx - wid / 2, starty - hei / 2, wid, hei);
+}
 
 Application.Run(form);
